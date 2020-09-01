d679c4f6ad0c5e8ae22e08aa45cbaca222c9adff
Barrett Pope <barrettpope94@gmail.com>
2020-09-01 11:41:11 -0400

    Functions created for selection options

--- employeeTracker.js
+++ employeeTracker.js
@@ -63,3 +63,158 @@ function start() {
             }
         });
 }
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        else {
+            return results;
+        };
+    });
+}
+
+function employeesByDepartment() {
+    inquirer
+        .prompt({
+            name: "employeeDepartment",
+            type: "list",
+            message: "Select Department ID",
+            choices: ["1", "2", "3", "4"]
+        })
+        .then(function (answer) {
+            if (answer.employeeDepartment === "1") {
+
+            } else if (answer.employeeDepartment === "2") {
+
+            } else if (answer.employeeDepartment === "3") {
+
+            } else if (answer.employeeDepartment === "4") {
+
+            } else {
+                connection.end();
+            }
+        });
+}
+
+function employeesByManager() {
+    inquirer
+        .prompt({
+            name: "employeeManager",
+            type: "list",
+            message: "Select Manager ID",
+            choices: ["Accounting", "Marketing", "Engineering", "Operations"]
+        })
+        .then(function (answer) {
+            if (answer.employeeDepartment === "Accounting") {
+
+            } else if (answer.employeeDepartment === "Marketing") {
+
+            } else if (answer.employeeDepartment === "Engineering") {
+
+            } else if (answer.employeeDepartment === "Operations") {
+
+            } else {
+                connection.end();
+            }
+        });
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([{
+                name: "id",
+                type: "input",
+                message: "Enter Employee ID"
+            },
+            {
+                name: "firstName",
+                type: "input",
+                message: "Enter Employee's First Name"
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "Enter Employee's Last Name"
+            },
+            {
+                name: "role",
+                type: "input",
+                message: "Enter Role ID"
+            },
+            {
+                name: "manager",
+                type: "input",
+                message: "Enter Manager ID"
+            }
+        ])
+        .then(function (answer) {
+            connection.query("INSERT INTO employee SET", {
+                id: answer.id,
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.role,
+                manager_id: answer.manager
+            })
+        })
+}
+
+function removeEmployee() {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt({
+                name: "employee",
+                type: "rawlist",
+                choices: function () {
+                    let choiceArray = [];
+                    for (let i = 0; i < results.length; i++) {
+
+                    }
+                },
+                message: "Which Employee Would You Like To Remove?"
+            })
+    })
+}
+
+function updateRole() {
+
+}
+
+function updateManager() {
+
+}
+
+function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, results) {
+        if (err) throw err;
+        else {
+            return results;
+        };
+    });
+}
+
+function addRole() {
+
+}
+
+function removeRole() {
+
+}
+
+function viewDepartments() {
+    connection.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        else {
+            return results;
+        };
+    });
+}
+
+function addDepartment() {
+
+}
+
+function removeDepartment() {
+
+}
